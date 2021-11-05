4ad4422ed88bf9d821434a16d9cd09dfc2cd5ad4
Jackson Almeida <jacksonnalmeida30@gmail.com>
2021-11-04 23:23:15 -0400

    Group by search

--- backend/src/main/java/com/dvsuperior/dsvendas/controller/SaleController.java
+++ backend/src/main/java/com/dvsuperior/dsvendas/controller/SaleController.java
@@ -1,5 +1,7 @@
 package com.dvsuperior.dsvendas.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dvsuperior.dsvendas.dto.SaleDTO;
+import com.dvsuperior.dsvendas.dto.SaleSucessDTO;
+import com.dvsuperior.dsvendas.dto.SaleSumDTO;
 import com.dvsuperior.dsvendas.servicies.SaleService;
 
 @RestController
@@ -23,5 +27,16 @@ public class SaleController {
 		Page<SaleDTO> listDTO = saleService.findAll(pageable);
 		return ResponseEntity.ok().body(listDTO);
 	}
-
+	
+	@GetMapping(value = "amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBy(){
+		List<SaleSumDTO> listDTO = saleService.amountGroupedBy();
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@GetMapping(value = "sucess-by-seller")
+	public ResponseEntity<List<SaleSucessDTO>> sucessGroupedBy(){
+		List<SaleSucessDTO> listDTO = saleService.sucessGroupedBy();
+		return ResponseEntity.ok().body(listDTO);
+	}
 }
--- backend/src/main/java/com/dvsuperior/dsvendas/servicies/SaleService.java
+++ backend/src/main/java/com/dvsuperior/dsvendas/servicies/SaleService.java
@@ -1,5 +1,7 @@
 package com.dvsuperior.dsvendas.servicies;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dvsuperior.dsvendas.dto.SaleDTO;
+import com.dvsuperior.dsvendas.dto.SaleSucessDTO;
+import com.dvsuperior.dsvendas.dto.SaleSumDTO;
 import com.dvsuperior.dsvendas.entities.Sale;
 import com.dvsuperior.dsvendas.repositories.SaleRepository;
 import com.dvsuperior.dsvendas.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> list = saleRepository.findAll(pageble);
 		return list.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBy(){
+		return saleRepository.amountGroupedBy();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSucessDTO> sucessGroupedBy(){
+		return saleRepository.sucessGroupedBy();
 	}
 }
