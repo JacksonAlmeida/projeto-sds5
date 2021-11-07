a59658e5b26b17f9baabb0c9a27e1488fa76cf68
Jackson Almeida <jacksonnalmeida30@gmail.com>
2021-11-07 15:35:14 -0400

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,34 +1,40 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
-    labels: string[]; 
+    labels: string[];
     series: number[];
 }
 
 const DonutChart = () => {
 
-    let chartData : ChartData = {labels: [], series: []};
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    axios.get(BASE_URL + '/sales/amount-by-seller')
-    .then(response => {
-        
-        const data = response.data as SaleSum[]; //castin]
-        const myLabel = data.map(x => x.sellerName);
-        const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        //let chartData : ChartData = {labels: [], series: []};
 
-        chartData = {labels: myLabel, series: mySeries};
+        axios.get(BASE_URL + '/sales/amount-by-seller')
+            .then(response => {
 
-        console.log(chartData);
-    });
-    //ou axios.get(crase${BASE_URL=sales/amount-by-seller crase});
+                const data = response.data as SaleSum[]; //castin]
+                const myLabel = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-    //const mockData = {
-    //  series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
+                setChartData({ labels: myLabel, series: mySeries });
+
+            });
+        //ou axios.get(crase${BASE_URL=sales/amount-by-seller crase});
+
+        //const mockData = {
+        //  series: [477138, 499928, 444867, 220426, 473088],
+        //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+        //}
+
+    }, []);
+
 
     const options = {
         legend: {
